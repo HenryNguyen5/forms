f33842e0df49f877c3133e7e58bd9866401e6ba1
henrynguyen5 <ngu.henry@hotmail.com>
2016-12-06 09:23:18 -0500

    udacity update last lesson

--- hello_world/main.py
+++ hello_world/main.py
@@ -14,26 +14,104 @@
 # limitations under the License.
 
 import webapp2
-form="""<form method="post" action="/testform">
-  <input name="q">
-  <input type="submit">
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+
+form = """
+<form method="post">
+    What is your birthday?
+    <br>
+
+    <label>
+      Month
+      <input type="text" name="month" value="%(month)s">
+    </label>
+
+    <label>
+      Day
+      <input type="text" name="day" value="%(day)s">
+    </label>
+
+    <label>
+      Year
+      <input type="text" name="year" value="%(year)s">
+    </label>
+
+    <div style="color: red">%(error)s</div>
+
+    <br>
+    <br>
+    <input type="submit">
 </form>
 """
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        #self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(form)
 
-class TestHandler(webapp2.RequestHandler):
+month_abbvs = dict((m[:3].lower(), m) for m in months)
+
+
+def valid_month(month):
+    if month:
+        month = month[:3].lower()
+        print month
+        return month_abbvs.get(month)
+
+
+def valid_day(day):
+    if day.isdigit() and int(day) > 0 and int(day) <= 31:
+        return int(day)
+
+
+def valid_year(year):
+    if year.isdigit() and int(year) >= 1950 and int(year) <= 2020:
+        return int(year)
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def write_fourm(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error,
+                                        "month": month,
+                                        "day": day,
+                                        "year": year})
+
+    def get(self):
+        self.write_fourm()
+
     def post(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        q = self.request.get("q")
-        self.response.write(q)
-        #this gets the http request
-        #self.response.out.write(self.request)
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
+
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+
+        if not (month and day and year):
+            # re-render form
+            self.write_fourm("That doesn't look right to me, friend.",
+                             user_month, user_day, user_year)
+        else:
+            self.redirect("/thanks")
+
+class ThanksHandler(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.out.write(
+            "Thanks! That's a totally valid day!")
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/testform',TestHandler)],
-     debug=True)
+    ('/thanks', ThanksHandler)],
+    debug=True)
